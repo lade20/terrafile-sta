8aedcb1a69044e43fdacd74a438aa34ec1418f41
User <user@lades.local>
2020-11-20 12:41:18 +0000

    adding new files

---
+++ hello1.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+terraform {
+    backend "s3" {
+        bucket = "terra-simple-bucket"
+        key    = "terra-simple1.tfstate"
+        region = "eu-west-1"
+    }
+}
+
+resource "aws_instance" "second_task" {
+  ami        = "ami-05626efdd7bf03b47"
+  instance_type = "t2.micro"
+  key_name = "devop22"
+}
+    
